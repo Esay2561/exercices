449cbf0402800f07c21856ba5e02c2e0d35dd144
Fatih <yalcinfatihtr@gmail.com>
2020-08-14 11:57:40 -0400

    first commit

---
+++ src/fdgfdhdf/Saaa.java
@@ -0,0 +1,15 @@
+package fdgfdhdf;
+
+import java.util.Scanner;
+
+public class Saaa {
+
+	public static void main(String[] args) {
+	System.out.println("hellooo");
+				}
+				
+				
+			
+				
+				
+	}
